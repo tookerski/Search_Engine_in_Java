14b1f203b4630e364d15809ce207924064ed8438
tookerski <fdliudaiyuan1986@163.com>
2016-08-31 23:03:54 +0800

    MODIFY httpclient retrivepage demo

--- src/com/tookerski/httpclient/demo/RetrivePage.java
+++ src/com/tookerski/httpclient/demo/RetrivePage.java
@@ -2,26 +2,29 @@ package com.tookerski.httpclient.demo;
 
 import java.io.IOException;
 
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class RetrivePage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpget = new HttpGet("https://github.com/tookerski");
 		try {
+			HttpGet httpget = new HttpGet("https://github.com/tookerski");
 			CloseableHttpResponse response1 = httpclient.execute(httpget);
-			System.out.println(response1.getStatusLine());
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			try {
+				System.out.println(response1.getStatusLine());
+				HttpEntity entity1 = response1.getEntity();
+				EntityUtils.consume(entity1);
+			} finally{
+				response1.close();
+			}
+		}  finally{
+			httpclient.close();
 		}
 
 	}
